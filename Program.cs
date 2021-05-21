3423d149df06198f9f087cc209f5cdf6c361d346
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-21 21:53:07 +0530

    basic project - works on my machine

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var inputPath = args[0];
+var outputPath = args[1];
+
+Console.WriteLine($"files: ");
+
+var filePaths = Directory.EnumerateFiles(inputPath);
+
+var fileData = filePaths.Select(p => new FileInfo(p));
+
+var filesGroupedByDate = fileData
+    .Select(p =>
+    new
+    {
+      FileName = p.Name,
+      Path = p.FullName,
+      CreatedAt = p.CreationTimeUtc
+    })
+    .GroupBy(p => p.CreatedAt);
+
+
+Console.WriteLine("Starting grouping...");
+filesGroupedByDate
+    .ToList()
+    .ForEach(p =>
+    {
+      var directoryPath = Path.Join(outputPath, String.Format("{0:yyyy-MM-dd}", p.Key));
+      Console.WriteLine($"Creating directory '{directoryPath}'");
+      Directory.CreateDirectory(directoryPath);
+
+      p.ToList().ForEach(file =>
+      {
+        var filePath = Path.Join(directoryPath, file.FileName);
+        Console.WriteLine($"Copying file '{file.Path}' to '{filePath}'");
+        File.Move(file.Path, filePath);
+      });
+    });
